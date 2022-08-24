beacf850565634283d288c6e0f1668e3c24f8882
madrigalar <madrigalar@gmail.com>
2022-08-24 10:51:18 -0500

    Se crea la clase Java llamada Empleado

---
+++ Empresa/src/Empresa/Empleado.java
@@ -0,0 +1,56 @@
+package Empresa;
+
+public class Empleado {
+
+    //Atributos
+
+
+    private String documentoEmpleado;
+    private String nombreEmpleado;
+    private String correoEmpleado;
+    private String rolEmpleado;
+
+    //Metodo constructor
+
+
+    public Empleado(String documentoEmpleado, String nombreEmpleado, String correoEmpleado, String rolEmpleado) {
+        this.documentoEmpleado = documentoEmpleado;
+        this.nombreEmpleado = nombreEmpleado;
+        this.correoEmpleado = correoEmpleado;
+        this.rolEmpleado = rolEmpleado;
+    }
+    // Setter y getters
+
+
+    public String getDocumentoEmpleado() {
+        return documentoEmpleado;
+    }
+
+    public void setDocumentoEmpleado(String documentoEmpleado) {
+        this.documentoEmpleado = documentoEmpleado;
+    }
+
+    public String getNombreEmpleado() {
+        return nombreEmpleado;
+    }
+
+    public void setNombreEmpleado(String nombreEmpleado) {
+        this.nombreEmpleado = nombreEmpleado;
+    }
+
+    public String getCorreoEmpleado() {
+        return correoEmpleado;
+    }
+
+    public void setCorreoEmpleado(String correoEmpleado) {
+        this.correoEmpleado = correoEmpleado;
+    }
+
+    public String getRolEmpleado() {
+        return rolEmpleado;
+    }
+
+    public void setRolEmpleado(String rolEmpleado) {
+        this.rolEmpleado = rolEmpleado;
+    }
+}
